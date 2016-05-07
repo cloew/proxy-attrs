5f62b16092add6e467fac3df9e7f231c72ed672a
Chris Loew <cloew123@gmail.com>
2016-05-07 16:25:23 -0600

    Added Boilerplate Package code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='proxy_attrs',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['proxy_attrs'],
+     )
