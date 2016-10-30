6576bd4d7df38886db0a605c22ae5dd8146629a7
Chris Loew <cloew123@gmail.com>
2016-10-30 16:35:44 -0600

    Added ability to specify a default value to use when the parent value is None

--- proxy_attrs.py
+++ proxy_attrs.py
@@ -1,24 +1,36 @@
 
-def proxy_for(parent, attrs):
+def proxy_for(parent, attrs, **kwargs):
     """ Add properties for the attrs on the provided field to 
         hide interaction from a class to an internal component """
     def addProxyData(cls):
         def add_property(cls, attr):
-            setattr(cls, attr, build_property(parent, attr))
+            setattr(cls, attr, ProxyAttr(parent, attr, **kwargs))
             
         for attr in attrs:
             add_property(cls, attr)
         return cls
     return addProxyData
     
-def ProxyAttr(parent, attr):
+def ProxyAttr(parent, attr, **kwargs):
     """ Return the property to get, set and delete the attr aon the parent field """
-    return build_property(parent, attr)
+    includeDefault = 'default' in kwargs
+    return build_property(parent, attr) if not includeDefault else build_default_property(parent, attr, kwargs['default'])
     
 def build_property(parent, attr):
     """ Return the property to access the field """
     def getter(self):
         return getattr(getattr(self, parent), attr)
+    def setter(self, v):
+        setattr(getattr(self, parent), attr, v)
+    def deleter(self):
+        delattr(getattr(self, parent), attr)
+    return property(getter, setter, deleter)
+    
+def build_default_property(parentAttr, attr, default):
+    """ Return the property to access the field or return a default value """
+    def getter(self):
+        parent = getattr(self, parentAttr)
+        return getattr(parent, attr) if parent is not None else default
     def setter(self, v):
         setattr(getattr(self, parent), attr, v)
     def deleter(self):
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='proxy_attrs',
-      version='0.1.1',
+      version='0.2.0',
       description="",
       author='Chris Loew',
       author_email='cloew123@gmail.com',
